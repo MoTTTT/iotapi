246ccac9d2d08d7a9386434f626ddf2bb9ee613f
Martin Colley <colleymj@telkom.co.za>
2024-01-25 16:30:44 +0000

    Spring initializr

---
+++ src/main/java/net/podzone/central/iotapi/IotapiApplication.java
@@ -0,0 +1,13 @@
+package net.podzone.central.iotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/podzone/central/iotapi/IotapiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.podzone.central.iotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
